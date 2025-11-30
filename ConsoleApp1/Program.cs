5abe26db38ddcdbe0ae2322db52000446c46b392
sqrsxc-droid <sqrsxc@gmail.com>
2025-11-30 18:43:07 +0900

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    static void Main()
+    {
+        MessageBox(IntPtr.Zero, "Windows работает нормально", "Message", 0);
+    }
+}
+
